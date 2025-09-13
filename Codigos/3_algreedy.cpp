f2582403ae0601d14a1b9ed0d90af032a68178c2
bielpieroli <joaogabrielpieroli18@gmail.com>
2025-09-12 23:59:36 -0300

    😀 3_alugreedy

--- Codigos/3_algreedy.cpp
+++ Codigos/3_algreedy.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int toMinutes(const string &hora) {
@@ -7,56 +8,81 @@ int toMinutes(const string &hora) {
     return h * 60 + m;
 }
 
-int main() {
-    int numberOfTests;
-    cin >> numberOfTests; 
+vector<vector<tuple<int, int, int>>> lerDados(int numberOfCarTypes, int numberOfRents) {
 
-    while (numberOfTests--) {
+    vector<vector<tuple<int, int, int>>> modelos(numberOfCarTypes + 1);
+    
+    for (int i = 0; i < numberOfRents; i++) {
+        int cliente, modelo;
+        string inicio, fim;
+        cin >> cliente >> inicio >> fim >> modelo;
+
+        int iniMinutes = toMinutes(inicio);
+        int endMinutes = toMinutes(fim);
+
+        modelos[modelo].push_back({endMinutes, iniMinutes, cliente});
+        // tupla é (fim, inicio, cliente)
+    }
+    
+    return modelos;
+}
+
+vector<int> processarModelo(vector<tuple<int, int, int>> &pedidos) {
+    sort(pedidos.begin(), pedidos.end(), [](const auto &a, const auto &b) {
+        if (get<0>(a) != get<0>(b)) // Primeiro, sort pelo fim
+            return get<0>(a) < get<0>(b);
+        else if (get<1>(a) != get<1>(b)) // Senão for pelo fim, é pelo começo
+            return get<1>(a) < get<1>(b);
+        else // Do contrário, é pelo cliente mesmo
+            return get<2>(a) < get<2>(b);
+    });
+
+    vector<int> escolhidos;
+    int ultimoFim = -1;
+
+    for (auto [fim, inicio, cliente] : pedidos) {
+        if (inicio >= ultimoFim) {
+            escolhidos.push_back(cliente);
+            ultimoFim = fim;
+        }
+    }
+
+    return escolhidos;
+}
+
+string formatarSaida(int modelo, const vector<int> &escolhidos) {
+
+    stringstream saida;    
+    saida << modelo << ": " << escolhidos.size();  
+    if (!escolhidos.empty()) saida << " = ";
+
+    for (size_t i = 0; i < escolhidos.size(); i++) {
+        if (i > 0) saida << ", ";
+        saida << escolhidos[i];
+    }
+
+    return saida.str();    
+}
+
+int main() {
+
+    int numberOfTests;
+    cin >> numberOfTests;
+
+    while (numberOfTests--) {   
+
         int numberOfCarTypes, numberOfRents;
         cin >> numberOfCarTypes >> numberOfRents;
 
-        vector<vector<tuple<int,int,int>>> modelos(numberOfCarTypes+1);
-        // cada tupla: (fim, inicio, cliente)
-
-        for (int i = 0; i < numberOfRents; i++) {
-            int cliente, modelo;
-            string inicio, fim;
-            cin >> cliente >> inicio >> fim >> modelo;
-
-            int iniMinutes = toMinutes(inicio);
-            int endMinutes = toMinutes(fim);
-
-            modelos[modelo].push_back({endMinutes, iniMinutes, cliente});
-        }
-
+        auto modelos = lerDados(numberOfCarTypes, numberOfRents);   
         vector<string> respostas;
 
         for (int modelo = 1; modelo <= numberOfCarTypes; modelo++) {
-            auto &pedidos = modelos[modelo];
-            sort(pedidos.begin(), pedidos.end());
-
-            vector<int> escolhidos;
-            int ultimoFim = -1;
-
-            for (auto [fim, inicio, cliente] : pedidos) {
-                if (inicio >= ultimoFim) {
-                    escolhidos.push_back(cliente);
-                    ultimoFim = fim;
-                }
-            }
-
-            // Monta saída para esse modelo
-            stringstream ss;
-            ss << modelo << ": " << escolhidos.size() << " = ";
-            for (size_t i = 0; i < escolhidos.size(); i++) {
-                if (i > 0) ss << ", ";
-                ss << escolhidos[i];
-            }
-            respostas.push_back(ss.str());
+            auto escolhidos = processarModelo(modelos[modelo]);
+            respostas.push_back(formatarSaida(modelo, escolhidos)); 
         }
 
-        // Imprime todos os modelos separados por " | "
-        for (size_t i = 0; i < respostas.size(); i++) {
+        for (size_t i = 0; i < respostas.size(); i++) { 
             if (i > 0) cout << " | ";
             cout << respostas[i];
         }
